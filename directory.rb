c4a9a356b4fd89e3daf300b4a206c36f1ca93d01
Tom Coakes <tomcoakes@gmail.com>
2015-01-27 14:51:13 +0000

    Wrapped all output into separate functions

--- directory.rb
+++ directory.rb
@@ -1,13 +1,19 @@
 # Let's put all students into an array
 students = ["Alex Parkinson", "Carrie Christenson", "Costas Sarris", "Diego Romero", "Erik Griffin", "Gabe Arafa", "Guido Vita Finzi", "Julian Veling", "Kate Beavis", "Kevin Lanzon", "Loris Fochesato", "Louise Beh", "Meads Chalcroft", "Paul Fitz", "Phil Brockwell", "Riz Ali", "Sean Haughton", "Sebastian Pires", "Thomas Strothjohann", "Tom Coakes", "Tommaso Bratto", "Vanessa Virgitti", "Yannick Brunner"]
 
-# And then print them
-puts "The students of my cohort at Makers Academy"
-puts "---------------"
-
-students.each do |student|
-  puts student
+def print_header
+  puts "The students of my cohort at Makers Academy"
+  puts "---------------"
 end
 
-# Finally we print the total
-puts "Overall, we have #{students.length} great students"
+def print(names)
+  names.each { |name| puts name }
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.length} great students"
+end
+
+print_header
+print(students)
+print_footer(students)
