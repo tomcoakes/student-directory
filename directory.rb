f98523568758a56863e7e275a8b5232821f29af3
Tom Coakes <tomcoakes@gmail.com>
2015-01-27 18:02:11 +0000

    Made the program interactive. students array is now generated by the user.

--- directory.rb
+++ directory.rb
@@ -1,29 +1,19 @@
 # Let's put all students into an array
-students = [
-  {:name => "Alex Parkinson", :cohort => :february},
-  {:name => "Carrie Christenson", :cohort => :february},
-  {:name => "Costas Sarris", :cohort => :february},
-  {:name => "Diego Romero", :cohort => :february},
-  {:name => "Erik Griffin", :cohort => :february},
-  {:name => "Gabe Arafa", :cohort => :february},
-  {:name => "Guido Vita Finzi", :cohort => :february},
-  {:name => "Julian Veling", :cohort => :february},
-  {:name => "Kate Beavis", :cohort => :february},
-  {:name => "Kevin Lanzon", :cohort => :february},
-  {:name => "Loris Fochesato", :cohort => :february},
-  {:name => "Louise Beh", :cohort => :february},
-  {:name => "Meads Chalcroft", :cohort => :february},
-  {:name => "Paul Fitz", :cohort => :february},
-  {:name => "Phil Brockwell", :cohort => :february},
-  {:name => "Riz Ali", :cohort => :february},
-  {:name => "Sean Haughton", :cohort => :february},
-  {:name => "Sebastian Pires", :cohort => :february},
-  {:name => "Thomas Strothjohann", :cohort => :february},
-  {:name => "Tom Coakes", :cohort => :february},
-  {:name => "Tommaso Bratto", :cohort => :february},
-  {:name => "Vanessa Virgitti", :cohort => :february},
-  {:name => "Yannick Brunner", :cohort => :february}
-]
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+
+  students = []
+  name = gets.chomp
+
+  while !name.empty? do
+    students << {:name => name, :cohort => :february}
+    puts "Now we have #{students.length} students"
+    name = gets.chomp
+  end
+
+  students
+end
 
 def print_header
   puts "The students of my cohort at Makers Academy"
@@ -38,6 +28,7 @@ def print_footer(names)
   puts "Overall, we have #{names.length} great students"
 end
 
+students = input_students
 print_header
 print(students)
 print_footer(students)
