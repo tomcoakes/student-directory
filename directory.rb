e86c63f9c98aa7840a80a03cf3f0b95b25858b30
Tom Coakes <tomcoakes@gmail.com>
2015-01-27 14:45:57 +0000

    Replaced the puts statements with an array and iterator

--- directory.rb
+++ directory.rb
@@ -1,29 +1,13 @@
-student_count = 23
-# First we print the list of students
+# Let's put all students into an array
+students = ["Alex Parkinson", "Carrie Christenson", "Costas Sarris", "Diego Romero", "Erik Griffin", "Gabe Arafa", "Guido Vita Finzi", "Julian Veling", "Kate Beavis", "Kevin Lanzon", "Loris Fochesato", "Louise Beh", "Meads Chalcroft", "Paul Fitz", "Phil Brockwell", "Riz Ali", "Sean Haughton", "Sebastian Pires", "Thomas Strothjohann", "Tom Coakes", "Tommaso Bratto", "Vanessa Virgitti", "Yannick Brunner"]
+
+# And then print them
 puts "The students of my cohort at Makers Academy"
-puts "-------------"
-puts "Alex Parkinson"
-puts "Carrie Christenson"
-puts "Costas Sarris"
-puts "Diego Romero"
-puts "Erik Griffin"
-puts "Gabe Arafa"
-puts "Guido Vita Finzi"
-puts "Julian Veling"
-puts "Kate Beavis"
-puts "Kevin Lanzon"
-puts "Loris Fochesato"
-puts "Louise Beh"
-puts "Meads Chalcroft"
-puts "Paul Fitz"
-puts "Phil Brockwell"
-puts "Riz Ali"
-puts "Sean Haughton"
-puts "Sebastian Pires"
-puts "Thomas Strothjohann"
-puts "Tom Coakes"
-puts "Tommaso Bratto"
-puts "Vanessa Virgitti"
-puts "Yannick Brunner"
+puts "---------------"
+
+students.each do |student|
+  puts student
+end
+
 # Finally we print the total
-puts "Overall, we have #{student_count} great students"
+puts "Overall, we have #{students.length} great students"
