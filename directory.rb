87b7d53dddbbc6387e4e55d07c6a8bca5ee9b74e
Tom Coakes <tomcoakes@gmail.com>
2015-01-28 11:48:20 +0000

    Removed all uses of chomp method, and replaced with a method of my own named #remove_return_char

--- directory.rb
+++ directory.rb
@@ -1,11 +1,15 @@
 # Let's put all students into an array
+def remove_return_char(string)
+  string.gsub(/\n/, '')
+end
+
 def input_students
   puts "Please enter the name of the student, press return, then enter their cohort and press return again."
   puts "To finish, just hit return twice"
 
   students = []
-  name = gets.chomp
-  month = gets.chomp.to_sym
+  name = remove_return_char(gets)
+  month = remove_return_char(gets).to_sym
 
   month = :February if month.empty?
 
@@ -13,8 +17,8 @@ def input_students
     students << {:name => name, :cohort => month, :bootcamp => :makersacademy}
     puts "Now we have #{students.length} student" if students.length == 1
     puts "Now we have #{students.length} students" if students.length > 1
-    name = gets.chomp
-    month = gets.chomp.to_sym
+    name = remove_return_char(gets)
+    month = remove_return_char(gets).to_sym
     month = :February if month.empty?
   end
   students
